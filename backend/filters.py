e711146f8bae5d7ec7a2e9600c88149263289ec0
KotelnikovKP <kotelnikov1302@gmail.com>
2023-06-28 13:55:42 +0500

    Expand filter options
    Add filtering on all fields to Equipment and EquipmentType (parameter 'q').

--- backend/filters.py
+++ backend/filters.py
@@ -1,43 +1,57 @@
+from django.db.models import Q
 from django_filters import rest_framework as filters
 
 from backend.models import EquipmentType, Equipment
 
 
-def filter_name(queryset, name, value):
+def filter_equipment_type_q(queryset, name, value):
+    return queryset.filter(Q(name__icontains=value) | Q(serial_number_mask__icontains=value))
+
+
+def filter_equipment_type_name(queryset, name, value):
     return queryset.filter(name__icontains=value)
 
 
-def filter_serial_number_mask(queryset, name, value):
-    return queryset.filter(serial_number_mask__contains=value)
+def filter_equipment_type_serial_number_mask(queryset, name, value):
+    return queryset.filter(serial_number_mask__icontains=value)
 
 
 class EquipmentTypeFilter(filters.FilterSet):
     """
         Фильтры для списка типов оборудования
 
-        Оба поля (name, serial_number_mask) с фильтрацией по вхождению
+        Общее поле q и оба поля (name, serial_number_mask) с фильтрацией по вхождению
     """
+    q = \
+        filters.CharFilter(label='Equipment type name or serial number mask for result set filtering '
+                                 '(by content case insensitive).',
+                           method=filter_equipment_type_q)
     name = \
         filters.CharFilter(label='Equipment type name for result set filtering (by content case insensitive).',
-                           method=filter_name)
+                           method=filter_equipment_type_name)
     serial_number_mask = \
-        filters.CharFilter(label='Serial number mask for result set filtering (by content case sensitive).',
-                           method=filter_serial_number_mask)
+        filters.CharFilter(label='Serial number mask for result set filtering (by content case insensitive).',
+                           method=filter_equipment_type_serial_number_mask)
 
     class Meta:
         model = EquipmentType
         fields = ['name', 'serial_number_mask']
 
 
-def filter_equipment_type(queryset, name, value):
+def filter_equipment_q(queryset, name, value):
+    return queryset.filter(Q(equipment_type__name__icontains=value) | Q(serial_number__icontains=value) |
+                           Q(description__icontains=value))
+
+
+def filter_equipment_equipment_type(queryset, name, value):
     return queryset.filter(equipment_type__name__icontains=value)
 
 
-def filter_serial_number(queryset, name, value):
+def filter_equipment_serial_number(queryset, name, value):
     return queryset.filter(serial_number__icontains=value)
 
 
-def filter_description(queryset, name, value):
+def filter_equipment_description(queryset, name, value):
     return queryset.filter(description__icontains=value)
 
 
@@ -45,17 +59,21 @@ class EquipmentFilter(filters.FilterSet):
     """
         Фильтры для списка оборудования
 
-        Поля (equipment_type_name, serial_number, description) с фильтрацией по вхождению
+        Общее поле q и поля (equipment_type_name, serial_number, description) с фильтрацией по вхождению
     """
+    q = \
+        filters.CharFilter(label='Equipment type name or serial number or description for result set filtering '
+                                 '(by content case insensitive).',
+                           method=filter_equipment_q)
     equipment_type_name = \
         filters.CharFilter(label='Equipment type name for result set filtering (by content case insensitive).',
-                           method=filter_equipment_type)
+                           method=filter_equipment_equipment_type)
     serial_number = \
         filters.CharFilter(label='Serial number for result set filtering (by content case insensitive).',
-                           method=filter_serial_number)
+                           method=filter_equipment_serial_number)
     description = \
         filters.CharFilter(label='Description for result set filtering (by content case insensitive).',
-                           method=filter_description)
+                           method=filter_equipment_description)
 
     class Meta:
         model = Equipment
